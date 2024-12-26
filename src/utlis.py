838339298358206f7fd018840377f9d4147ff702
Youssef Lyousfi <youcef.lyousfi@gmail.com>
2024-12-26 15:16:27 +0100

    Utils modified

--- src/utlis.py
+++ src/utlis.py
@@ -1,16 +1,30 @@
 import base64
-from IPython.display import Image, display
+from IPython.display import Image, display, Markdown
 
+def query_response(chain):
+    question = input("Enter your question: ")
+    response = chain.invoke(question)
 
-def get_images_base64(chunks):
-    images_b64 = []
-    for chunk in chunks:
-        if "CompositeElement" in str(type(chunk)):
-            chunk_els = chunk.metadata.orig_elements
-            for el in chunk_els:
-                if "Image" in str(type(el)):
-                    images_b64.append(el.metadata.image_base64)
-    return images_b64
+    # Styling using HTML in Markdown
+    styled_output = f"""
+    <div style="font-family: Arial, sans-serif; line-height: 1.6; margin-top: 20px;">
+        <p style="color: #2c3e50; font-size: 18px; font-weight: bold;">
+            Question:
+        </p>
+        <p style="background-color: #ecf0f1; color: #34495e; padding: 10px; border-radius: 5px;">
+            {question}
+        </p>
+        <p style="color: #2c3e50; font-size: 18px; font-weight: bold;">
+            Response:
+        </p>
+        <p style="background-color: #dff9fb; color: #1e272e; padding: 10px; border-radius: 5px;">
+            {response}
+        </p>
+    </div>
+    """
+
+    # Display styled output
+    display(Markdown(styled_output))
 
 
 def display_base64_image(base64_code):
