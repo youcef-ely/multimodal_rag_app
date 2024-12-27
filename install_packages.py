d1719cf202856886cd3720bb55bbf7a7fc3b359b
Youssef Lyousfi <youcef.lyousfi@gmail.com>
2024-12-27 19:24:24 +0100

    Install packages edited

--- install_packages.py
+++ install_packages.py
@@ -5,7 +5,11 @@ commands = [
     'pip install -Uq chromadb tiktoken --quiet --progress-bar off',
     'pip install -Uq langchain langchain-community langchain-groq --quiet --progress-bar off',
     'pip install -Uq python_dotenv --quiet --progress-bar off',
-    'sudo apt-get install poppler-utils tesseract-ocr libmagic-dev'
+    'sudo apt-get install poppler-utils tesseract-ocr libmagic-dev',
+    'pip install langchain_huggingface --quiet --progress-bar off',
+    'pip install langchain-chroma --quiet --progress-bar off',
+    'pip install streamlit --quiet --progress-bar off',
+    'pip install -U langchain-ollama --quiet'
 ]
 
 for command in commands:
