74a4e28139b2de76853ef0ef9b821e46e64dfb6d
Youssef Lyousfi <youcef.lyousfi@gmail.com>
2025-01-02 00:33:11 +0100

    install packages edited

--- src/install_packages.py
+++ src/install_packages.py
@@ -13,7 +13,9 @@ commands = [
     'pip install langchain_huggingface --quiet --progress-bar off',
     'pip install langchain-chroma --quiet --progress-bar off',
     'pip install streamlit --quiet --progress-bar off',
-    'pip install -U langchain-ollama --quiet'
+    'pip install -U langchain-ollama --quiet',
+    'pip install evaluate rouge_score --quiet',
+    'pip install pretty_errors --quiet',
 ]
 
 for command in tqdm(commands, desc='Installing packages'):
